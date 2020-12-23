667effc057deb44d77ab8d338c9883e1fa0a7549
mschop <mschopdev@gmail.com>
2020-12-23 12:05:21 +0100

    Fix command migration:create

--- src/Migration/Commands/CreateCommand.php
+++ src/Migration/Commands/CreateCommand.php
@@ -5,7 +5,6 @@ namespace Phespro\Phespro\Migration\Commands;
 
 
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -18,7 +17,7 @@ class CreateCommand extends Command
         $this->setDescription('Create migration in directory');
 
         $this->addOption('directory', 'd', InputOption::VALUE_REQUIRED, 'Where should the migration file be created?', getcwd());
-        $this->addOption('namespace', 'n', InputOption::VALUE_REQUIRED, 'Namespace of the generated class');
+        $this->addOption('namespace', 's', InputOption::VALUE_REQUIRED, 'Namespace of the generated class');
         $this->addOption('description', 'e', InputOption::VALUE_OPTIONAL, 'Description of the migration', '');
     }
 
@@ -28,14 +27,14 @@ class CreateCommand extends Command
         $namespace = $input->getOption('namespace');
         $description = $input->getOption('description');
         $description = str_replace("'", '\\\'', $description);
-        $id = (new \DateTimeImmutable())->format('YmdHisu') . bin2hex(random_bytes(5));
+        $id = (new \DateTimeImmutable())->format('YmdHisu') . random_int(0, 9) . random_int(0, 9) . random_int(0, 9));
         $className = "Migration$id";
 
         $code = "<?php
 
 namespace $namespace;
 
-use Phespro\Phespro\MigrationInterface;
+use Phespro\Phespro\Migration\MigrationInterface;
 
 class $className implements MigrationInterface 
 {
