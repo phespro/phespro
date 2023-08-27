25358260a3ed4cce5fa21e713df21474accf0110
mschop <mschopdev@gmail.com>
2023-08-27 17:51:41 +0200

    Add AjaxOnlyMiddleware

---
+++ src/Http/Middlewares/AjaxOnlyMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Phespro\Phespro\Http\Middlewares;
+
+use Laminas\Diactoros\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class AjaxOnlyMiddleware implements MiddlewareInterface
+{
+    protected const HEADER_NAME = 'HTTP_X_REQUESTED_WITH';
+    protected const EXPECTED_HEADER_VALUE = 'XMLHttpRequest';
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $isAjax = $request->getHeader(self::HEADER_NAME) === self::EXPECTED_HEADER_VALUE;
+        if (!$isAjax) {
+            return $handler->handle($request);
+        } else {
+            $response = new Response(status: 400);
+            $response->getBody()->write('Only AJAX requests are allowed for this method');
+            return $response;
+        }
+    }
+}
