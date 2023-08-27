eb3a858246ddec6fd426229a1777c7d83648c865
mschop <mschopdev@gmail.com>
2023-08-27 09:44:13 +0200

    Add __invoke to ActionInterface

--- src/Http/ActionInterface.php
+++ src/Http/ActionInterface.php
@@ -2,7 +2,10 @@
 
 namespace Phespro\Phespro\Http;
 
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 interface ActionInterface
 {
-
+    public function __invoke(ServerRequestInterface $request): ResponseInterface;
 }
