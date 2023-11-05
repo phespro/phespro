c8167855b8b9193b6f9ed75f6eb4395b23b17501
mschop <mschopdev@gmail.com>
2023-11-05 17:23:02 +0100

    Add MysqlMigrationStateStorage

---
+++ src/Migration/MigrationStateStorage/MysqlMigrationStateStorage.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Phespro\Phespro\Migration\MigrationStateStorage;
+
+use Phespro\Phespro\Migration\MigrationStateStorageInterface;
+
+readonly class MysqlMigrationStateStorage implements MigrationStateStorageInterface
+{
+    public function __construct(
+        protected \PDO $pdo,
+    )
+    {
+    }
+
+    public function add(string $id): void
+    {
+        $this->pdo->prepare("
+            INSERT INTO migration(id)
+            VALUES (:id)
+        ")->execute(['id' => $id]);
+    }
+
+    public function contains(string $id): bool
+    {
+        $stmt = $this->pdo->prepare("SELECT COUNT(*) FROM migration WHERE id = :id");
+        $stmt->execute(['id' => $id]);
+        return !!$stmt->fetchColumn();
+    }
+
+    public function getPassed(): iterable
+    {
+        return $this->pdo->query("SELECT id FROM migration")->fetchAll(\PDO::FETCH_COLUMN);
+    }
+
+    public function prepareDataStructures(): void
+    {
+        $this->pdo->exec("
+            CREATE TABLE IF NOT EXISTS migration (
+                id VARCHAR(255) PRIMARY KEY,
+            );     
+        ");
+    }
+}
